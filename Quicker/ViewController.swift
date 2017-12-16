6a8de1a0055d7d0e33caec0d0d687a2cb3da7e99
Yuto Iida <iida.yuto@digitalgrid.com>
2017-12-16 14:06:18 +0900

    Initial Commit

---
+++ Quicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quicker
+//
+//  Created by 飯田悠斗 on 16/12/2017.
+//  Copyright © 2017 Meeeesi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
