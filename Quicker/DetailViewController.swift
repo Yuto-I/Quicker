e4046614b80915e891ff148334e8ab2a2cdbaa15
Yuto Iida <iida.yuto@digitalgrid.com>
2017-12-16 14:23:03 +0900

    Arrange the minimum objects on the storyboard.

---
+++ Quicker/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  Quicker
+//
+//  Created by 飯田悠斗 on 16/12/2017.
+//  Copyright © 2017 Meeeesi. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
